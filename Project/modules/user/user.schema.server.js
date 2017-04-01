31c6ad58230cd0122aa8baf8d9f0fe8a8dd44d24
fibinfa <francisassissi.f@husky.neu.edu>
2017-04-01 15:01:08 -0400

    user schema

---
+++ Project/modules/user/user.schema.server.js
@@ -0,0 +1,30 @@
+module.exports = function () {
+    var mongoose =  require("mongoose");
+    var BusinessSchema = require("../business/business.schema.server")();
+
+    var UserSchema = mongoose.Schema({
+        username: {type: String, required: true},
+        role: {type: String, enum: ['CUSTOMER', 'OWNER', 'CRITIC', 'ADMIN'],default: 'CUSTOMER'},
+        password: String,
+        firstName: String,
+        lastName: String,
+        email: String,
+        phone: String,
+        company: String,
+        url: String,
+        businesses: {type:mongoose.Schema.Types.ObjectId, ref: 'BusinessModel'},
+        dateCreated: {type: Date, default: Date.now()},
+        facebook: {
+            id:    String,
+            token: String,
+            displayName: String
+        },
+        google: {
+            token: String,
+            id: String,
+            displayName: String
+        }
+    }, {collection: "user"});
+
+    return UserSchema;
+};

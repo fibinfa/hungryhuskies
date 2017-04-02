83b4fea32a843623bbb453ce04e685db9e005e4d
Sankar Gireesan Nair <sankargireesan@ccs.neu.edu>
2017-04-02 03:42:04 -0400

    homepage

--- Project/public/config.js
+++ Project/public/config.js
@@ -1,7 +1,18 @@
 (function () {
     angular
         .module("HungryApp")
+
+
+        // .config(function($locationProvider) {
+        //     $locationProvider.hashPrefix('!');})
+        //
+        // .run(function($location) {
+        //     $location.path('/');
+        // })
+
+
         .config(configuration);
+
 
     function configuration($routeProvider) {
         $routeProvider
@@ -13,92 +24,92 @@
             })
 
 
-            .when("/login",{
+            .when("/login", {
                 templateUrl: 'views/user/templates/login.view.client.html'
                 // controller: 'LoginController',
                 // controllerAs: 'model'
             })
-            .when("/yelp",{
+            .when("/yelp", {
                 templateUrl: "views/yelp.html",
                 controller: "YelpSearchController",
                 controllerAs: "model"
             });
-            // .when("/",{
-            //     templateUrl: 'views/user/templates/login.view.client.html',
-            //     controller: 'LoginController',
-            //     controllerAs: 'model'
-            // })
-            // .when("/register",{
-            //     templateUrl: 'views/user/templates/register.view.client.html',
-            //     controller: 'RegisterController',
-            //     controllerAs: 'model'
-            // })
-            // .when("/user",{
-            //     templateUrl: 'views/user/templates/profile.view.client.html',
-            //     controller: 'ProfileController',
-            //     controllerAs: 'model',
-            //     resolve: {
-            //         checkLoggedIn: checkLoggedIn
-            //     }
-            // })
-            // .when("/user/:uid",{
-            //     templateUrl: 'views/user/templates/profile.view.client.html',
-            //     controller: 'ProfileController',
-            //     controllerAs: 'model',
-            //     resolve: {
-            //         checkLoggedIn: checkLoggedIn
-            //     }
-            // })
-            // .when("/user/:uid/website",{
-            //     templateUrl: 'views/website/templates/website-list.view.client.html',
-            //     controller: "WebsiteListController",
-            //     controllerAs: "model"
-            // })
-            // .when("/user/:uid/website/new",{
-            //     templateUrl: 'views/website/templates/website-new.view.client.html',
-            //     controller: "NewWebsiteController",
-            //     controllerAs: "model"
-            // })
-            // .when("/user/:uid/website/:wid",{
-            //     templateUrl: 'views/website/templates/website-edit.view.client.html',
-            //     controller: "EditWebsiteController",
-            //     controllerAs: "model"
-            // })
-            // .when("/user/:uid/website/:wid/page",{
-            //     templateUrl: 'views/page/templates/page-list.view.client.html',
-            //     controller: "PageListController",
-            //     controllerAs: "model"
-            // })
-            // .when("/user/:uid/website/:wid/page/new",{
-            //     templateUrl: 'views/page/templates/page-new.view.client.html',
-            //     controller: "NewPageController",
-            //     controllerAs: "model"
-            // })
-            // .when("/user/:uid/website/:wid/page/:pid",{
-            //     templateUrl: 'views/page/templates/page-edit.view.client.html',
-            //     controller: "EditPageController",
-            //     controllerAs: "model"
-            // })
-            // .when("/user/:uid/website/:wid/page/:pid/widget",{
-            //     templateUrl: 'views/widget/templates/widget-list.view.client.html',
-            //     controller: "WidgetListController",
-            //     controllerAs: "model"
-            // })
-            // .when("/user/:uid/website/:wid/page/:pid/widget/new",{
-            //     templateUrl: 'views/widget/templates/widget-chooser.view.client.html'
-            //     ,controller: "NewWidgetController",
-            //     controllerAs: "model"
-            // })
-            // .when("/user/:uid/website/:wid/page/:pid/widget/:wgid",{
-            //     templateUrl: 'views/widget/templates/widget-edit.view.client.html'
-            //     ,controller: "EditWidgetController",
-            //     controllerAs: "model"
-            // })
-            // .when("/user/:userId/website/:wid/page/:pid/widget/:wgid/flickr",{
-            //     templateUrl: "views/widget/templates/widget-flickr-search.view.client.html",
-            //     controller: "FlickrImageSearchController",
-            //     controllerAs: "model"
-            // });
+        // .when("/",{
+        //     templateUrl: 'views/user/templates/login.view.client.html',
+        //     controller: 'LoginController',
+        //     controllerAs: 'model'
+        // })
+        // .when("/register",{
+        //     templateUrl: 'views/user/templates/register.view.client.html',
+        //     controller: 'RegisterController',
+        //     controllerAs: 'model'
+        // })
+        // .when("/user",{
+        //     templateUrl: 'views/user/templates/profile.view.client.html',
+        //     controller: 'ProfileController',
+        //     controllerAs: 'model',
+        //     resolve: {
+        //         checkLoggedIn: checkLoggedIn
+        //     }
+        // })
+        // .when("/user/:uid",{
+        //     templateUrl: 'views/user/templates/profile.view.client.html',
+        //     controller: 'ProfileController',
+        //     controllerAs: 'model',
+        //     resolve: {
+        //         checkLoggedIn: checkLoggedIn
+        //     }
+        // })
+        // .when("/user/:uid/website",{
+        //     templateUrl: 'views/website/templates/website-list.view.client.html',
+        //     controller: "WebsiteListController",
+        //     controllerAs: "model"
+        // })
+        // .when("/user/:uid/website/new",{
+        //     templateUrl: 'views/website/templates/website-new.view.client.html',
+        //     controller: "NewWebsiteController",
+        //     controllerAs: "model"
+        // })
+        // .when("/user/:uid/website/:wid",{
+        //     templateUrl: 'views/website/templates/website-edit.view.client.html',
+        //     controller: "EditWebsiteController",
+        //     controllerAs: "model"
+        // })
+        // .when("/user/:uid/website/:wid/page",{
+        //     templateUrl: 'views/page/templates/page-list.view.client.html',
+        //     controller: "PageListController",
+        //     controllerAs: "model"
+        // })
+        // .when("/user/:uid/website/:wid/page/new",{
+        //     templateUrl: 'views/page/templates/page-new.view.client.html',
+        //     controller: "NewPageController",
+        //     controllerAs: "model"
+        // })
+        // .when("/user/:uid/website/:wid/page/:pid",{
+        //     templateUrl: 'views/page/templates/page-edit.view.client.html',
+        //     controller: "EditPageController",
+        //     controllerAs: "model"
+        // })
+        // .when("/user/:uid/website/:wid/page/:pid/widget",{
+        //     templateUrl: 'views/widget/templates/widget-list.view.client.html',
+        //     controller: "WidgetListController",
+        //     controllerAs: "model"
+        // })
+        // .when("/user/:uid/website/:wid/page/:pid/widget/new",{
+        //     templateUrl: 'views/widget/templates/widget-chooser.view.client.html'
+        //     ,controller: "NewWidgetController",
+        //     controllerAs: "model"
+        // })
+        // .when("/user/:uid/website/:wid/page/:pid/widget/:wgid",{
+        //     templateUrl: 'views/widget/templates/widget-edit.view.client.html'
+        //     ,controller: "EditWidgetController",
+        //     controllerAs: "model"
+        // })
+        // .when("/user/:userId/website/:wid/page/:pid/widget/:wgid/flickr",{
+        //     templateUrl: "views/widget/templates/widget-flickr-search.view.client.html",
+        //     controller: "FlickrImageSearchController",
+        //     controllerAs: "model"
+        // });
         // function checkLoggedIn($q, UserService,$location) {
         //     var deferred = $q.defer();
         //     UserService
@@ -117,4 +128,8 @@
         // }
 
     }
+
+
+
+
 })();

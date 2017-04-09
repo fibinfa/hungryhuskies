7c510482a78d15078d46188b2b5d16d8d106e5e3
fibinfa <francisassissi.f@husky.neu.edu>
2017-04-09 15:49:50 -0400

    q

--- Project/public/views/restaurant/controller/restaurant-details.controller.client.js
+++ Project/public/views/restaurant/controller/restaurant-details.controller.client.js
@@ -3,6 +3,8 @@
         .module("HungryApp")
         .controller("RestaurantDetailController", RestaurantDetailController);
 
+
+
     function RestaurantDetailController($routeParams,RestaurantService,$rootScope) {
         var vm = this;
         var restaurantId = $routeParams.rid;
@@ -11,45 +13,55 @@
 
 
 
-    function init() {
-        vm.currentUser = $rootScope.currentUser;
+
+        function init() {
+            vm.currentUser = $rootScope.currentUser;
 
 
-        RestaurantService
-            .findRestaurantByIdYelp(restaurantId)
-            .then(
-                function (res) {
-                    console.log(res);
-                    vm.data = res.data;
-                }, function (err) {
-                    console.log(err);
-                }
-            );
-
-        // findBusiness();
+            RestaurantService
+                .findRestaurantByIdYelp(restaurantId)
+                .then(
+                    function (res) {
+                        // console.log(res.data.location.coordinate.latitude);
+                        vm.data = res.data;
 
 
-    }
+                        console.log(vm.data);
+                        // $scope.lat =res.data.location.coordinate.latitude;
+                        // $scope.lng =res.data.location.coordinate.longitude;
+                        // console.log($scope.lat);
+                        // console.log($scope.lng);
+                    }, function (err) {
+                        console.log(err);
+                    }
+                );
 
-    // function findBusiness() {
-    //     BusinessService
-    //         .findBusinessById(businessId)
-    //         .then(
-    //             function (res) {
-    //                 vm.localBusiness = res.data;
-    //             }
-    //         );
-    // }
+            // findBusiness();
 
-    // function search(businessId, businessArray) {
-    //     for (var i=0; i < businessArray.length; i++) {
-    //         if (businessArray[i]._id === businessId) {
-    //             return true;
-    //         }
-    //     }
-    //     return false;
-    // }
-    init();
+
+
+        }
+
+        // function findBusiness() {
+        //     BusinessService
+        //         .findBusinessById(businessId)
+        //         .then(
+        //             function (res) {
+        //                 vm.localBusiness = res.data;
+        //             }
+        //         );
+        // }
+
+        // function search(businessId, businessArray) {
+        //     for (var i=0; i < businessArray.length; i++) {
+        //         if (businessArray[i]._id === businessId) {
+        //             return true;
+        //         }
+        //     }
+        //     return false;
+        // }
+        init();
+
 
 
 
@@ -116,5 +128,27 @@
 
         }
 
+
     }
+
+//     function initMap() {
+//
+//
+//             var scope = angular.element("#map").scope();
+//             var lt =  window.lat;
+//             var lg =  window.lng;
+//
+//             console.log(window.lat);
+//             var uluru = {lat: lt, lng: lg};
+// //        console.log(scope)
+//             var map = new google.maps.Map(document.getElementById('map'), {
+//                 zoom: 8,
+//                 center: uluru
+//             });
+//             var marker = new google.maps.Marker({
+//                 position: uluru,
+//                 map: map
+//             });
+//         }
+
 })();

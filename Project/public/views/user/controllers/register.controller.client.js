bf3e46d5946854dcea02a61e2f1a0faf04e18b22
Sankar Gireesan Nair <sankargireesan@ccs.neu.edu>
2017-04-12 19:39:36 -0400

    register page tweet

--- Project/public/views/user/controllers/register.controller.client.js
+++ Project/public/views/user/controllers/register.controller.client.js
@@ -5,30 +5,35 @@
     
     function registerController(UserService,$location, $rootScope) {
         var vm=this;
-        vm.createUser=createUser;
+        // vm.createUser=createUser;
         vm.register=register;
 
-        function createUser(user){
-            console.log(user);
+        function register(user){
+            // console.log(user);
             UserService
                 .createUser(user)
                 .success(function (user) {
+                    console.log(user);
                     $rootScope.currentUser = user;
                     $location.url("/user");
+                })
+                .error(function(err){
+                    vm.error = err;
+                    createUser(user);
                 });
         }
         
-        function register(user) {
-            UserService
-                .findUserByUsername(user.username)
-                .success(function (user) {
-                    vm.error = "Username already exists";
-                })
-                .error(function(err){
-                    vm.message = "Available";
-                    createUser(user);
-                })
-        }
+        // function register(user) {
+        //     UserService
+        //         .findUserByUsername(user.username)
+        //         .success(function (user) {
+        //             vm.error = "Username already exists";
+        //         })
+        //         .error(function(err){
+        //             vm.message = "Available";
+        //             createUser(user);
+        //         })
+        // }
 
     }
 })();
